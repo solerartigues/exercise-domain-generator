dec70fc5f956455ca6a0bb8f010dedff320f4c28
Carlos Soler <carlossolerartigues@gmail.com>
2022-03-10 14:19:31 +0000

    Ejercicio Terminado Domain Generator

---
+++ app.js
@@ -0,0 +1,17 @@
+let dominios = () => {
+
+    var parte1 = ["the", "our"];
+    var parte2 = ["great", "big"];
+    var parte3 = ["jogger", "racoon"];
+    
+    for (i = 0; i < parte1.length; i++) {
+        for (j = 0; j < parte2.length; j++) {
+            for (k = 0; k < parte2.length; k++) {
+                console.log(parte1[i]+ parte2[j]+ parte3[k]+".com")
+            }
+        }
+    }
+ }
+
+ console.log(dominios())
+ 
